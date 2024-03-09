8029bf672d9d4471cefdc5193587ebe5c7aa1ab5
EvanVink <evink2@my.bcit.ca>
2024-03-08 21:47:40 -0800

    update

---
+++ Assignment4/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const fs = require("fs");
+
+
+app.use("/js", express.static("./public/js"));
+app.use("/css", express.static("./public/css"));
+
+
+app.get("/", (req, res) => {
+    let doc = fs.readFileSync("./page/index.html", "utf8");
+    res.send(doc);
+})
+
+app.get("/hello", function (req, res){
+    res.send("<html><body>hello world!</body></html>");
+})
+
+
+
+
+
+
+
+
+
+let port = 8000;
+app.listen(port, function() {
+    console.log("This Page is Listening on port " + port + "!")
+});
